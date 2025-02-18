a35e2d086658c04320bb085acec8034b995456ff
VadimRukavitsyn <vadimrukavitsyn@yandex.ru>
2025-02-18 12:32:56 +0300

    Update and rename vadim.js to main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+function writehead(){
+  document.write('''<div class="head">''')
+  document.write('''<p class="p"><a class="href" href="index1.html">Химия</a></p>''')
+  document.write('''<p class="p"><a class="href" href="index2.html">Биология</a></p>''')
+  document.write('''<p class="p"><a class="href" href="index3.html">Математика</a></p>''')
+  document.write('''</div>''')               
+}
