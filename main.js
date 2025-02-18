c65b0953395859fe550bb1618245d087758bf37a
VadimRukavitsyn <vadimrukavitsyn@yandex.ru>
2025-02-18 20:23:57 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,35 @@
 //function writehead(){
   //Контейнер-шапка
-    let head = document.createElement('div');
+    var head = document.createElement('div');
     head.className = "head";
     head.innerHTML = "<div></div>";
     document.body.append(head);
     //Элемент 1 для вставки ссылки
-    let p1 = document.createElement('p');
+    var p1 = document.createElement('p');
     p1.className = "p";
     head.appendChild(p1);
     //Элемент 2 для вставки ссылки
-    let p2 = document.createElement('p');
+    var p2 = document.createElement('p');
     p2.className = "p";
     head.appendChild(p2);
     //Элемент 3 для вставки ссылки
-    let p3 = document.createElement('p');
+    var p3 = document.createElement('p');
     p3.className = "p";
     head.appendChild(p3);
     //Ссылка на химию
-    let a1 = document.createElement('a');
+    var a1 = document.createElement('a');
     a1.className = "href";
     a1.href="https://vadimrukavitsyn.github.io/index1.html";
     a1.innerHTML = "<a>Химия</a>";
     p1.append(a1);
     //Ссылка на биологию
-    let a2 = document.createElement('a');
+    var a2 = document.createElement('a');
     a2.className = "href";
     a2.href="https://vadimrukavitsyn.github.io/index2.html";
     a2.innerHTML = "<a>Биология</a>";
     p2.append(a2);
     //Ссылка на математику
-    let a3 = document.createElement('a');
+    var a3 = document.createElement('a');
     a3.className = "href";
     a3.href="https://vadimrukavitsyn.github.io/index3.html";
     a3.innerHTML = "<a>Математика</a>";
