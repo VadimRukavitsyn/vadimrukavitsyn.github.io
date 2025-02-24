5f9e9aad97361e2e0b265aea77e87f37224b6078
VadimRukavitsyn <vadimrukavitsyn@yandex.ru>
2025-02-24 09:35:44 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 function div_biology_close()
 {document.getElementById('div_biology_list').style.display = 'none';
-}
+};
+function div_mathematics_close()
+{document.getElementById('div_mathematics_list').style.display = 'none';
+};
 function HeadCreate() {var head = document.createElement('div');
 head.className = "head";
 document.body.insertBefore(head, document.body.firstChild);
@@ -18,12 +21,6 @@ div_biology.className = "div-head";
 div_biology.id = "div_biology";
 div_biology.innerHTML = '<a class="href" href="https://vadimrukavitsyn.github.io/index2.html">Биология</a>';
 head.appendChild(div_biology);
-
-var div_mathematics = document.createElement('div');
-div_mathematics.className = "div-head";
-div_mathematics.id = "div_mathematics";
-div_mathematics.innerHTML = '<a class="href" href="https://vadimrukavitsyn.github.io/mathematics.html">Математика и информатика</a>';
-head.appendChild(div_mathematics);
 
 var div_biology_list = document.createElement('div');
 div_biology_list.className = "div-list";
@@ -55,14 +52,60 @@ a_biology_addition.href = "https://vadimrukavitsyn.github.io/biology/addition.ht
 a_biology_addition.className = "href";
 a_biology_addition.innerHTML = "Дополнительная информация";
 div_biology_list.appendChild(a_biology_addition);
+
+var div_mathematics = document.createElement('div');
+div_mathematics.className = "div-head";
+div_mathematics.id = "div_mathematics";
+div_mathematics.innerHTML = '<a class="href" href="https://vadimrukavitsyn.github.io/mathematics.html">Математика и информатика</a>';
+head.appendChild(div_mathematics);
+
+var div_mathematics_list = document.createElement('div');
+div_mathematics_list.className = "div-list";
+div_mathematics_list.id = "div_mathematics_list";
+div_mathematics.appendChild(div_mathematics_list);
+
+var a_mathematics_statistics = document.createElement('a');
+a_mathematics_statistics.href = "https://vadimrukavitsyn.github.io/mathematics/statistics.html";
+a_mathematics_statistics.className = "href";
+a_mathematics_statistics.innerHTML = "Статистика";
+div_mathematics_list.appendChild(a_mathematics_statistics);
+
+var a_programming_languages = document.createElement('a');
+a_programming_languages.href = "https://vadimrukavitsyn.github.io/mathematics/programming-languages.html";
+a_programming_languages.className = "href";
+a_programming_languages.innerHTML = "Языки программирования";
+div_mathematics_list.appendChild(a_programming_languages);
+
+var a_machine_learning = document.createElement('a');
+a_machine_learning.href = "https://vadimrukavitsyn.github.io/mathematics/machine-learning.html";
+a_machine_learning.className = "href";
+a_machine_learning.innerHTML = "Машинное обучение";
+div_mathematics_list.appendChild(a_machine_learning);
+
+var a_modeling = document.createElement('a');
+a_modeling.href = "https://vadimrukavitsyn.github.io/mathematics/modeling.html";
+a_modeling.className = "href";
+a_modeling.innerHTML = "Моделирование";
+div_mathematics_list.appendChild(a_modeling);
+
 /*Задачи по цитологии*/
 document.getElementById('div_biology').onmouseover = function()
 {
   document.getElementById('div_biology_list').style.display = 'grid';
+  document.getElementById('div_mathematics_list').style.display = 'none';
 };
 document.getElementById('div_biology').onmouseout = function()
 {
   setTimeout(div_biology_close, 4000)
+};
+document.getElementById('div_mathematics').onmouseover = function()
+{
+  document.getElementById('div_mathematics_list').style.display = 'grid';
+  document.getElementById('div_biology_list').style.display = 'none';
+};
+document.getElementById('div_mathematics').onmouseout = function()
+{
+  setTimeout(div_mathematics_close, 4000)
 };
 }
 HeadCreate();
